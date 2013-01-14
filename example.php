0174b6671b376c1d1f350234f6c91130a7d82922
GrizzLyCRO <grizzlycro@gmail.com>
2013-01-14 19:43:08 +0100

    Remove stric standard notice far calling dynamic method statically.

--- example.php
+++ example.php
@@ -3,12 +3,13 @@
 require_once('Fiskalizator.php');
 $fis = new Fiskalizator();
 
-$fis->certPath = 'path/to/demo/demo.pfx';
-$fis->certPass = 'mypass';
+$fis->certPath = 'demo.pfx';
+$fis->certPass = 'pass';
 $fis->CisUrl = 'https://cistest.apis-it.hr:8449/FiskalizacijaServiceTest';
 
+$doc = new DOMDocument();
 $xml_string = file_get_contents('racun.xml');
-$doc = DOMDocument::loadXML($xml_string);
+$doc->loadXML($xml_string);
 
 $response = $fis->doRequest($doc);
 
