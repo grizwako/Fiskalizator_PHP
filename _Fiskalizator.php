7b2ee845a0c41504ecc3621272b4ee76b38613f4
grizwako <grizzlycro@gmail.com>
2013-01-25 16:46:50 +0100

    dummy

---
+++ _Fiskalizator.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Korisnik3
+ * Date: 23.01.13.
+ * Time: 13:46
+ * To change this template use File | Settings | File Templates.
+ */
+
+class _Fiskalizator {
+
+}
