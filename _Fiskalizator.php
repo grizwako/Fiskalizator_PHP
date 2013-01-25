acea6fab27665e57c6a0b88631bf9b6b9c990390
grizwako <grizzlycro@gmail.com>
2013-01-25 16:47:16 +0100

    a

--- _Fiskalizator.php
+++ _Fiskalizator.php
@@ -1,12 +1,12 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Korisnik3
- * Date: 23.01.13.
- * Time: 13:46
- * To change this template use File | Settings | File Templates.
- */
 
 class _Fiskalizator {
+
+    /** @var FiskalRequestXML */
+    private $request;
+
+    /** @var FiskalResponseXML */
+    private $response;
+
 
 }
