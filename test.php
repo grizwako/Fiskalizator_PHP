522a645a1ad299f8727f846e41aa879c81ad436a
grizwako <grizzlycro@gmail.com>
2013-01-28 11:01:12 +0100

    Important comments

--- test.php
+++ test.php
@@ -8,20 +8,21 @@ $doc->formatOutput = true;
 $xml_string = file_get_contents('racun.xml');
 $doc->loadXML($xml_string);
 
+
 $fis = new _Fiskalizator('certificates/demo/my_private.pfx', 'pass');
 
+#UNCOMMENT FOLLOWING LINE AFTER YOU THOROUGHLY TESTED DEMO MODE (service provider says 2 days minimum)
 #$fis->setProductionMode();
+#Also, do not forget to change certPath and certPass to match your production certificate
 
 try {
     $fis->doRequest($doc);
-
     #custom timeout and number of retries on network error, default is 3 retries and 5 seconds timeout tolerance
     #$fis->doRequest($doc, 10, 5.2);
 } catch (Exception $e) {
     echo $e->getMessage();
     die();
 }
-
 
 echo 'Success<br>';
 if ($fis->getRequestType() == 'RacunZahtjev'){
